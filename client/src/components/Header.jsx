fbd975fa47b39828b77e20903ccd80e46ec22dda
ankitt251 <89956629+ankitt251@users.noreply.github.com>
2024-06-26 18:29:55 +0530

    update the header and make the profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,16 @@
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
+  const { currentUser } = useSelector(state => state.user);
   return (
-    <header className="bg-slate-200 shadow-md">
+    <header className="bg-zinc-300 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Ankit</span>
-            <span className="text-slate-700">Estate</span>
+          <h1 className="font-bold text-sm sm:text-2xl flex flex-wrap font-serif">
+            <span className="text-indigo-400">Ankit</span>
+            <span className="text-rose-600">Estate</span>
           </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
@@ -19,20 +21,24 @@ export default function Header() {
           />
           <FaSearch className="text-slate-600" />
         </form>
-        <ul className="flex gap-4">
-            <Link to='/'>
-          <li className="hidden sm:inline text-slate-700 hover:underline">
-            Home
-          </li>
-            </Link>
-            <Link to='/about'>
-          <li className="hidden sm:inline text-slate-700 hover:underline">
-            About
-          </li>
-            </Link>
-            <Link to='/sign-in'>
-          <li className="text-slate-700 hover:underline">Sign In</li>
-            </Link>
+        <ul className="flex gap-6 text-lg font-mono">
+          <Link to="/">
+            <li className="hidden sm:inline text-indigo-400 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-indigo-400 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/profile">
+            {currentUser ? (
+              <img className="rounded-full h-8 w-8 object-cover" src={currentUser.avatar} alt="profile"/>
+            ) : (
+              <li className="text-indigo-400 hover:underline">Sign In</li>
+            )}
+          </Link>
         </ul>
       </div>
     </header>
