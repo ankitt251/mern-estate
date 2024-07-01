311964209ea15b63660e721dbdaedee7510a0866
ankitt251 <89956629+ankitt251@users.noreply.github.com>
2024-07-01 14:05:56 +0530

    same

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,4 @@
-import { set } from "mongoose";
-import { list } from "postcss";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
@@ -12,8 +10,8 @@ export default function Home() {
   const [offerListings, setOfferListings] = useState([]);
   const [saleListings, setSaleListings] = useState([]);
   const [rentListings, setRentListings] = useState([]);
-  console.log(saleListings);
   SwiperCore.use([Navigation]);
+  console.log(offerListings);
   useEffect(() => {
     const fetchOfferListings = async () => {
       try {
@@ -42,7 +40,7 @@ export default function Home() {
         const data = await res.json();
         setSaleListings(data);
       } catch (error) {
-        console.log(error);
+        log(error);
       }
     };
     fetchOfferListings();
@@ -50,7 +48,6 @@ export default function Home() {
   return (
     <div>
       {/* top */}
-
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
         <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
           Find your next <span className="text-slate-500">perfect</span>
@@ -58,20 +55,20 @@ export default function Home() {
           place with ease
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm">
-          Ankit Estate is the best place to find your next perfect place to live
+          Sahand Estate is the best place to find your next perfect place to
+          live.
           <br />
           We have a wide range of properties for you to choose from.
         </div>
         <Link
-          className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
           to={"/search"}
+          className="text-xs sm:text-sm text-blue-800 font-bold hover:underline"
         >
           Let's get started...
         </Link>
       </div>
 
       {/* swiper */}
-
       <Swiper navigation>
         {offerListings &&
           offerListings.length > 0 &&
@@ -83,20 +80,20 @@ export default function Home() {
                   backgroundSize: "cover",
                 }}
                 className="h-[500px]"
-                key={list._id}
+                key={listing._id}
               ></div>
             </SwiperSlide>
           ))}
       </Swiper>
 
-      {/* Listing results for offer, sale and rent */}
+      {/* listing results for offer, sale and rent */}
 
-      <div className="flex flex-col max-w-6xl mx-auto p-3 gap-8 my-10">
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10">
         {offerListings && offerListings.length > 0 && (
-          <div>
+          <div className="">
             <div className="my-3">
               <h2 className="text-2xl font-semibold text-slate-600">
-                Recent Offers
+                Recent offers
               </h2>
               <Link
                 className="text-sm text-blue-800 hover:underline"
@@ -113,7 +110,7 @@ export default function Home() {
           </div>
         )}
         {rentListings && rentListings.length > 0 && (
-          <div>
+          <div className="">
             <div className="my-3">
               <h2 className="text-2xl font-semibold text-slate-600">
                 Recent places for rent
@@ -133,7 +130,7 @@ export default function Home() {
           </div>
         )}
         {saleListings && saleListings.length > 0 && (
-          <div>
+          <div className="">
             <div className="my-3">
               <h2 className="text-2xl font-semibold text-slate-600">
                 Recent places for sale
