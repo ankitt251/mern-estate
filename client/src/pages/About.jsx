e04366079600807051d52ae9fcbd612f88cdb90f
ankitt251 <89956629+ankitt251@users.noreply.github.com>
2024-07-01 15:02:25 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About Ankit Estate</h1>
+      <p className="mb-4 text-slate-700">
+        Welcome to Ankit Estate, where your dream property awaits. At Ankit
+        Estate, we pride ourselves on offering a personalized and seamless real
+        estate experience, tailored to meet the unique needs of each client.
+        Whether you are looking to buy, sell, or invest, our team of dedicated
+        professionals is here to guide you through every step of the process.
+        With years of experience in the industry and an in-depth understanding
+        of the local market, we are committed to helping you find the perfect
+        property that fits your lifestyle and budget.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our mission at Ankit Estate is to provide exceptional service and build
+        lasting relationships with our clients. We believe that every
+        transaction is more than just a deal; it’s a new beginning, a fresh
+        start, and a stepping stone towards your future. That’s why we take the
+        time to listen to your needs, understand your goals, and work tirelessly
+        to achieve them. Our comprehensive range of services includes
+        residential and commercial property sales, property management, and
+        investment consulting, ensuring that we can meet all your real estate
+        needs under one roof.
+      </p>
+      <p className="mb-4 text-slate-700">
+        At Ankit Estate, we are passionate about real estate and dedicated to
+        excellence. Our team stays abreast of the latest market trends and
+        utilizes cutting-edge technology to provide you with the most accurate
+        and up-to-date information. We are proud to have built a reputation for
+        integrity, transparency, and client satisfaction. When you choose Ankit
+        Estate, you are not just choosing a real estate agency; you are choosing
+        a partner who will stand by your side and work in your best interest
+        every step of the way.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Thank you for considering Ankit Estate. We look forward to the
+        opportunity to help you achieve your real estate dreams and to welcome
+        you to our family of satisfied clients. Let us help you turn your real
+        estate aspirations into reality.
+      </p>
+    </div>
+  );
 }
