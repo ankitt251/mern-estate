6edad6d04ab517f3dbc0ab9529b8bea820720e3b
ankitt251 <89956629+ankitt251@users.noreply.github.com>
2024-07-01 19:14:58 +0530

    Changes

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -288,7 +288,7 @@ export default function CreateListing() {
                 type="number"
                 id="regularPrice"
                 min="50"
-                max="10000000"
+                max="10000000000000"
                 required
                 className="p-3 border border-gray-300 rounded-lg"
                 onChange={handleChange}
@@ -307,7 +307,7 @@ export default function CreateListing() {
                   type="number"
                   id="discountPrice"
                   min="0"
-                  max="10000000"
+                  max="100000000000000"
                   required
                   className="p-3 border border-gray-300 rounded-lg"
                   onChange={handleChange}
